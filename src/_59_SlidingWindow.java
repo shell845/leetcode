266ce54c41a9743cd3c313c0aa42a8c58797fd61
shell845 <4274250+shell845@users.noreply.github.com>
2020-04-18 15:04:36 +0800

    reverse string, sliding window for nums, day17

---
+++ src/_59_SlidingWindow.java
@@ -0,0 +1,73 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * @author YC (shell845)
+ * @date 18/4/2020 12:55 PM
+ */
+
+public class _59_SlidingWindow {
+    /** leetcode #239
+     * * time complexity O(n), space O(n) */
+    public static int[] slidingWindowMax(final int[] in, final int w) {
+        final int[] max_left = new int[in.length];
+        final int[] max_right = new int[in.length];
+
+        max_left[0] = in[0];
+        max_right[in.length - 1] = in[in.length - 1];
+
+        for (int i = 1; i < in.length; i++) {
+            max_left[i] = (i % w == 0) ? in[i] : Math.max(max_left[i - 1], in[i]);
+
+            final int j = in.length - i - 1;
+            max_right[j] = (j % w == 0) ? in[j] : Math.max(max_right[j + 1], in[j]);
+        }
+
+        final int[] sliding_max = new int[in.length - w + 1];
+        for (int i = 0, j = 0; i + w <= in.length; i++) {
+            sliding_max[j++] = Math.max(max_right[i], max_left[i + w - 1]);
+        }
+
+        return sliding_max;
+    }
+
+    /** use double open ended queue (deque)
+     * time complexity O(n), space O(n)*/
+    public int[] maxSlidingWindow_2(int[] nums, int k) {
+        if (nums == null || nums.length == 0) return new int[0];
+        int[] ans = new int[nums.length - k + 1];
+        Deque<int[]> deque = new ArrayDeque<>();
+        int idx = 0;
+        for (int i = 0; i < nums.length; i++) {
+            // add new element to queue
+            while (!deque.isEmpty() && deque.peekLast()[0] < nums[i]) {
+                deque.pollLast();
+            }
+            deque.add(new int[]{nums[i], i});
+            // remove out of window elements from queue
+            while (deque.peekFirst()[1] <= i - k) {
+                deque.pollFirst();
+            }
+            // add max num in window to answer
+            if (i >= k - 1 && !deque.isEmpty()) {
+                ans[idx] = deque.peekFirst()[0];
+                idx++;
+            }
+        }
+        return ans;
+    }
+
+    /** time complexity O(nk), space O(n)*/
+    public int[] maxSlidingWindow_1(int[] nums, int k) {
+        if (nums == null || nums.length == 0) return new int[0];
+        int[] ans = new int[nums.length - k + 1];
+        for (int i = 0, idx = 0; i < nums.length - k + 1; i++, idx++) {
+            int temp = nums[i];
+            for (int j = 1; j < k; j++) {
+                temp = Math.max(temp, nums[i + j]);
+            }
+            ans[idx] = temp;
+        }
+        return ans;
+    }
+}
