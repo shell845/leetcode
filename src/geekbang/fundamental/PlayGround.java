a6349bb81a569c6d1c9a45a87ad2c7fc7b0de614
shell845 <4274250+shell845@users.noreply.github.com>
2021-02-03 10:14:51 -0500

    28

--- src/geekbang/fundamental/PlayGround.java
+++ src/geekbang/fundamental/PlayGround.java
@@ -1,9 +1,6 @@
 package geekbang.fundamental;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author YC (shell845)
@@ -11,9 +8,75 @@ import java.util.List;
  */
 
 public class PlayGround {
+     public static class Ball {
+         int num;
+         Ball(int n) {num = n;}
+    }
+
+    static class Parent {
+        String song = "Parent";
+        Ball ball = new Ball(5);
+
+        public void sing() {
+            System.out.println("Parent sings " + song + " and wins " + ball.num + " balls");
+        }
+
+        public void sing(String song) {
+            System.out.println("Parent sings " + song);
+        }
+
+        public static void staticSing() {
+            System.out.println("Parent static song ");
+        }
+
+        public static void staticSing(String song) {
+            System.out.println("Parent static song " + song);
+        }
+    }
+
+    static class Child extends Parent {
+        String song = "Child";
+        Ball ball = new Ball(10);
+
+        @Override
+        public void sing() {
+            System.out.println("Child sings " + song + " and wins " + ball.num + " balls");
+        }
+
+        public void sing(String song) {
+            System.out.println("Child sings " + song);
+        }
+
+        public static void staticSing() {
+            System.out.println("Child static song");
+        }
+
+        public static void staticSing(String song) {
+            System.out.println("Child static song " + song);
+        }
+    }
 
     public static void main(String args[]) {
-        boolean b = false;
-        System.out.println(!b);
+         System.out.println("Test main overload");
+    }
+
+
+    public static void main(Integer i) {
+        Parent parent = new Child();
+        System.out.println(parent.song);
+        parent.sing();
+        parent.sing("a song");
+        parent.staticSing();
+        parent.staticSing("2 songs");
+        System.out.println(parent.ball.num);
+
+
+        Object o = "abc";
+        String s = (String) o;
+        System.out.println(s);
+        System.out.println(o instanceof String);
+        System.out.println(o.hashCode());
+
+
     }
 }
